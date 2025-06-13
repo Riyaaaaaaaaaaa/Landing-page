4e115858bc40cd0e108a7fbedcf7f5ea4ad3b259
Riya Ann Varghese <121488007+Riyaaaaaaaaaaa@users.noreply.github.com>
2025-06-13 11:15:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-const playlists = {
-  happy: {
-    sunny: ["'Walking on Sunshine' - Katrina & The Waves", "'Uptown Funk' - Bruno Mars"],
-    rainy: ["'Banana Pancakes' - Jack Johnson", "'I'm Yours' - Jason Mraz"],
-    cloudy: ["'Happy' - Pharrell Williams", "'Can't Stop the Feeling!' - Justin Timberlake"],
-    snowy: ["'Let It Go' - Idina Menzel", "'Winter Wonderland' - Michael Bublé"]
-  },
-  sad: {
-    sunny: ["'Fix You' - Coldplay", "'Tears Dry on Their Own' - Amy Winehouse"],
-    rainy: ["'Someone Like You' - Adele", "'The Night We Met' - Lord Huron"],
-    cloudy: ["'Breathe Me' - Sia", "'Skinny Love' - Bon Iver"],
-    snowy: ["'River' - Joni Mitchell", "'Back to December' - Taylor Swift"]
-  },
-  chill: {
-    sunny: ["'Sunflower' - Rex Orange County", "'Electric Feel' - MGMT"],
-    rainy: ["'Coffee' - Miguel", "'Come Away With Me' - Norah Jones"],
-    cloudy: ["'Lo-fi beats to relax/study to'", "'Weightless' - Marconi Union"],
-    snowy: ["'Holocene' - Bon Iver", "'Cherry Wine' - Hozier"]
-  },
-  energetic: {
-    sunny: ["'Stronger' - Kanye West", "'Can't Hold Us' - Macklemore"],
-    rainy: ["'Run the World' - Beyoncé", "'Thunder' - Imagine Dragons"],
-    cloudy: ["'Titanium' - David Guetta", "'Don't Stop Believin'' - Journey"],
-    snowy: ["'Eye of the Tiger' - Survivor", "'Shake It Off' - Taylor Swift"]
-  }
-};
 
 function generatePlaylist() {
   const mood = document.getElementById("mood").value;
